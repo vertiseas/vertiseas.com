f4a474eaef42d34a76272f64449fea4fb75d8e64
Vertiseas <115049552+Vertiseas@users.noreply.github.com>
2024-11-28 02:21:53 -0500

    Added abripiano

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,22 +3,37 @@
 const vertfart1 = `Assets/vertfart1.mp3`;
 const vertfart2 = `Assets/vertfart2.mp3`;
 const vertfart3 = `Assets/vertfart3.mp3`;
+const abripiano = new Audio(`Assets/abripiano.mp3`);
+const abri = document.querySelector(".abri");
+const abrifish = document.querySelector(".abrifish");
 
 const farts = [vertfart1, vertfart2, vertfart3];
 
 const derpvert = document.getElementById("derpvert");
 
 derpvert.addEventListener("click", () => {
-  // Randomly select one of the fart files
   const randomFart = farts[Math.floor(Math.random() * farts.length)];
 
-  // Create a new Audio instance and play it
   const fartSound = new Audio(randomFart);
   fartSound.play();
 
-  // Add shake animation
   derpvert.classList.add("derpvert-shake");
   setTimeout(() => {
     derpvert.classList.remove("derpvert-shake");
   }, 700);
+});
+
+const abriclick = document.getElementById("abriclick");
+
+abriclick.addEventListener("click", () => {
+  abri.classList.toggle("hidden");
+  abripiano.load();
+  setTimeout(() => {
+    abrifish.classList.add("abri-play");
+    abripiano.play();
+  }, 2000);
+  setTimeout(() => {
+    abri.classList.toggle("hidden");
+    abrifish.classList.remove("abri-play");
+  }, 9000);
 });
